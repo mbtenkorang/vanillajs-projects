8c3454b9e63ed797f41a8e92a1a5b6f566fb8148
Moses <mbtenkorang@yahoo.com>
2023-02-17 16:46:48 +0000

    Add event listener to <buttons>

---
+++ counter-app/app.js
@@ -0,0 +1,22 @@
+//Set initlal count
+let count = 0;
+
+// Select value annd buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      //   console.log("decrease");
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else if (styles.contains("reset")) {
+      //   console.log("reset", count);
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
