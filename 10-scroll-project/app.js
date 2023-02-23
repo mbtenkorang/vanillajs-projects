169525ad834e4f19d5ce489472491b87a51d3271
Moses <mbtenkorang@yahoo.com>
2023-02-23 15:42:08 +0000

    Final Project

---
+++ 10-scroll-project/app.js
@@ -0,0 +1,87 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ********** set date ************
+
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  //Toggle css property
+  // linksContainer.classList.toggle("show-links");
+
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  // console.log(containerHeight);
+  const linksHeight = links.getBoundingClientRect().height;
+  // console.log(linksHeight);
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+// ********** fixed navbar ************
+const navBar = document.getElementById("nav");
+const toplink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  let scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fixed-nav");
+  } else {
+    navBar.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 750) {
+    toplink.classList.add("show-link");
+  } else {
+    toplink.classList.remove("show-link");
+  }
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    //prevent default
+    e.preventDefault();
+
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    // Calculate heights
+    const navbarHeight = navBar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navBar.classList.contains("fixed-nav");
+
+    let position = element.offsetTop - navbarHeight;
+    // console.log(position);
+
+    if (!fixedNav) {
+      position = position - navbarHeight;
+    }
+
+    if (navbarHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
